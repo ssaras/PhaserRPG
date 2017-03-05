57b6e7ba80a1c700f6ad1f9c96e111d748fc9c6d
Shawn Saras <shawnsaras@gmail.com>
2017-03-05 07:50:51 -0500

    Game.Level1 cleaned up

--- js/Level1.js
+++ js/Level1.js
@@ -1,53 +1,40 @@
-Game.Level1 = function (game) { };
-
-var map;
-var layer;
-
-var player;
-var controls = {};
-var playerSpeed = 175;
-var jumpTimer = 0;
-
-var music;
+Game.Level1 = function (game) { 
+    this.map;
+    this.layer;
+    this.player;
+    this.controls = {};
+    this.playerSpeed = 175;
+    this.music;
+};
 
 Game.Level1.prototype = {
     create: function () {
 
-        this.stage.backgroundColor = "#0000ff";
-
-        map = this.add.tilemap("map", 32, 32);
+        this.map = this.add.tilemap("map", 32, 32);        
+        this.map.addTilesetImage("tileset");
+        this.map.setCollisionBetween(2,2);
         
-        map.addTilesetImage("tileset");
-        
-        layer = map.createLayer(0);
-        
-        layer.resizeWorld();
+        this.layer = this.map.createLayer(0);
+        this.layer.resizeWorld();       
 
-        map.setCollisionBetween(2,2);
+        this.music = this.add.audio("worldmap");
+        this.music.loopFull();
 
-        music = this.add.audio("worldmap");
+        this.player = this.add.sprite(0, 200, "player");
+        this.player.frame = 58;
+        this.player.anchor.setTo(0.5, 0.5);
 
-        music.loopFull();
+        this.player.animations.add("idle", [58], 7, true);
+        this.player.animations.add("runDown", [57,58,59], 7, true);
+        this.player.animations.add("runLeft", [69,70,71], 7, true);
+        this.player.animations.add("runRight", [81, 82, 83], 7, true);
+        this.player.animations.add("runUp", [93,94,95], 7, true);
 
-        player = this.add.sprite(0, 200, "player");
-        player.frame = 58;
-        player.anchor.setTo(0.5, 0.5);
+        this.camera.follow(this.player);
+        this.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
 
-        player.animations.add("idle", [58], 7, true);
-        player.animations.add("runDown", [57,58,59], 7, true);
-        player.animations.add("runLeft", [69,70,71], 7, true);
-        player.animations.add("runRight", [81, 82, 83], 7, true);
-        player.animations.add("runUp", [93,94,95], 7, true);
-
-        // player.animations.add("idle", [0,1], 1, true);
-        // player.animations.add("jump", [2], 1, true);
-        // player.animations.add("run", [3,4,5,6,7,8], 7, true);
-
-        this.physics.arcade.enable(player);
-        this.camera.follow(player);
-        player.body.collideWorldBounds = true;
-
-        controls = {
+        this.controls = {
             right: this.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
             left: this.input.keyboard.addKey(Phaser.Keyboard.LEFT),
             up: this.input.keyboard.addKey(Phaser.Keyboard.UP),
@@ -56,34 +43,34 @@ Game.Level1.prototype = {
     },
 
     update: function () {
-        this.physics.arcade.collide(player, layer);
+        this.physics.arcade.collide(this.player, this.layer);
 
-        // player.animations.play("idle");
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
+        // this.player.animations.play("idle");
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
 
-        if (controls.up.isDown) {
-            player.animations.play("runUp");
-            player.scale.setTo(1,1);
-            player.body.velocity.y -= playerSpeed;
+        if (this.controls.up.isDown) {
+            this.player.animations.play("runUp");
+            this.player.scale.setTo(1,1);
+            this.player.body.velocity.y -= this.playerSpeed;
         }
 
-        if (controls.down.isDown) {
-            player.animations.play("runDown");
-            player.scale.setTo(1,1);
-            player.body.velocity.y += playerSpeed;
+        if (this.controls.down.isDown) {
+            this.player.animations.play("runDown");
+            this.player.scale.setTo(1,1);
+            this.player.body.velocity.y += this.playerSpeed;
         }
 
-        if (controls.right.isDown) {
-            player.animations.play("runRight");
-            player.scale.setTo(1,1);
-            player.body.velocity.x += playerSpeed;
+        if (this.controls.right.isDown) {
+            this.player.animations.play("runRight");
+            this.player.scale.setTo(1,1);
+            this.player.body.velocity.x += this.playerSpeed;
         }
 
-        if (controls.left.isDown) {
-            player.animations.play("runLeft");
-            player.scale.setTo(1,1);
-            player.body.velocity.x -= playerSpeed;
+        if (this.controls.left.isDown) {
+            this.player.animations.play("runLeft");
+            this.player.scale.setTo(1,1);
+            this.player.body.velocity.x -= this.playerSpeed;
         }        
 
     }
